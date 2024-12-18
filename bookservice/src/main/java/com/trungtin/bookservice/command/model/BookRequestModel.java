65f5d5208946ceb27b7193f82386816a233d83f1
84In <trungtin7166293@gmail.com>
2024-12-18 14:40:41 +0700

    tạo employeeservice

--- bookservice/src/main/java/com/trungtin/bookservice/command/model/BookRequestModel.java
+++ bookservice/src/main/java/com/trungtin/bookservice/command/model/BookRequestModel.java
@@ -1,5 +1,8 @@
 package com.trungtin.bookservice.command.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Setter
@@ -8,7 +11,13 @@ import lombok.*;
 @AllArgsConstructor
 public class BookRequestModel {
     private String id;
+
+    @NotBlank(message = "Tên là bắt buộc")
+    @Size(min = 2, max = 50, message = "Tên phải bắt buộc từ 2 đến 30 kí tự")
     private String name;
+
+    @NotBlank(message = "Tác giả là bắt buộc")
     private String author;
+
     private Boolean isReady;
 }
--- commonservice/src/main/java/com/trungtin/commonservice/advice/ExceptionAdvice.java
+++ commonservice/src/main/java/com/trungtin/commonservice/advice/ExceptionAdvice.java
@@ -2,17 +2,32 @@ package com.trungtin.commonservice.advice;
 
 
 import com.trungtin.commonservice.model.ErrorMessage;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, DefaultMessageSourceResolvable::getDefaultMessage, (a, b) -> b));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleException(Exception ex) {
         return new ResponseEntity<>(new ErrorMessage("999",ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR),HttpStatus.INTERNAL_SERVER_ERROR);
-
     }
+
 }
